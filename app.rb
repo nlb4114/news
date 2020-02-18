a5778875fd82ff51b2e4a35036f479b3f6b53361
nlb4114 <nback24@gmail.com>
2020-02-18 18:03:39 +0000

    first attempt

--- app.rb
+++ app.rb
@@ -3,16 +3,60 @@ require "sinatra/reloader"
 require "geocoder"
 require "forecast_io"
 require "httparty"
+require 'news-api'
 def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
-# enter your Dark Sky API key here
-ForecastIO.api_key = "YOUR-API-KEY"
+
+
 
 get "/" do
-  # show a view that asks for the location
+  view "ask"
 end
 
 get "/news" do
-  # do everything else
+        results = Geocoder.search(params["q"])
+        lat_long = results.first.coordinates
+        lat = lat_long[0]
+        long = lat_long[1]
+        
+
+       # configure the Dark Sky API with your API key
+        ForecastIO.api_key = "ec96baecc5eb294ba0c7a9e5f369f9df"
+
+        forecast = ForecastIO.forecast(lat, long).to_hash
+        #"#{forecast}"
+
+#current_temp = forecast["currently"]["temperature"]
+#current_cond = forecast["currently"]["summary"]
+#for day in forecast["daily"]["data"]
+#end
+
+# Init
+newsapi = News.new("2487f35b8e7047ecbbc6edfe3c104c31")             
+
+# /v2/top-headlines
+top_headlines = newsapi.get_top_headlines(q: 'bitcoin',
+                                          sources: 'bbc-news,the-verge',
+                                          category: 'business',
+                                          language: 'en',
+                                          country: 'us')
+pp "#{top_headlines}"
+                                          
+    #view "news"
+
 end
+
+
+#url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=2487f35b8e7047ecbbc6edfe3c104c31"
+#news = HTTParty.get(url).parsed_response.to_hash
+# news is now a Hash you can pretty print (pp) and parse for your output
+# Init
+#newsapi = News.new("2487f35b8e7047ecbbc6edfe3c104c31")             
+
+# /v2/top-headlines
+#top_headlines = newsapi.get_top_headlines(q: 'bitcoin',
+                                          #sources: 'bbc-news,the-verge',
+                                          #category: 'business',
+                                          #language: 'en',
+                                          #country: 'us')
