16f630ad8f9810db1259b5f0512cd40379376180
nlb4114 <nback24@gmail.com>
2020-02-18 19:44:46 +0000

    adding bootstrap elements

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ get "/news" do
         long = lat_long[1]
         
 
-       # configure the Dark Sky API with your API key
+        # configure the Dark Sky API with your API key
         ForecastIO.api_key = "ec96baecc5eb294ba0c7a9e5f369f9df"
 
         forecast = ForecastIO.forecast(lat, long).to_hash
@@ -33,17 +33,15 @@ get "/news" do
 #end
 
 # Init
-newsapi = News.new("2487f35b8e7047ecbbc6edfe3c104c31")             
+#newsapi = News.new("2487f35b8e7047ecbbc6edfe3c104c31")             
 
 # /v2/top-headlines
-top_headlines = newsapi.get_top_headlines(q: 'bitcoin',
-                                          sources: 'bbc-news,the-verge',
-                                          category: 'business',
-                                          language: 'en',
-                                          country: 'us')
-pp "#{top_headlines}"
+url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=2487f35b8e7047ecbbc6edfe3c104c31"
+news = HTTParty.get(url).parsed_response.to_hash
+# news is now a Hash you can pretty print (pp) and parse for your output
+
                                           
-    #view "news"
+    view "news"
 
 end
 
